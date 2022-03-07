e668d211bab84476bebf2de1d47c45649846db0d
Vagner Matheus Da Cruz <vagnerdacruz48@gmail.com>
2022-03-07 18:14:52 -0300

    trabalhando com números no ruby

---
+++ ruby/02 - tipos de variáveis/3-numeros.rb
@@ -0,0 +1,13 @@
+soma = 5 + 3.2
+
+# imprimindo um número inteiro
+
+p 42
+
+# imprimindo um número com ponto flutuante
+
+p 42.42
+
+# Somando um inteiro com ponto flutuante (o resultado sempre será um float)
+
+p soma
